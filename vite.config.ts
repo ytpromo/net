b3900fd0bc55a256f94b6a7eb8cf5750015a8a25
Parham Afkar <parhamafkar12548@gmail.com>
2025-11-05 15:16:58 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: true // تمام هاست‌ها را允许 می‌کند
   }
 })
